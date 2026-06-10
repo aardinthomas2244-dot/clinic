a56bd2a33831165cd79e38e13593926258c15182
aardinthomas2244-dot <aardinthomas2244@gmail.com>
2026-06-10 22:08:25 +0530

    Create Appointment.java

---
+++ app/src/main/java/com/project/back_end/models/Appointment.java
@@ -0,0 +1,13 @@
+package com.project.back_end.models;
+
+import java.time.LocalDateTime;
+
+public class Appointment {
+    private int id;
+    private int patientId;
+    private int doctorId;
+    private LocalDateTime appointmentDate;
+    private String status;
+
+    // Constructor, getters, and setters will go here
+}
